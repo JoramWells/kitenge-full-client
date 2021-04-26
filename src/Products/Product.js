740c0a11e2cd0e4b88632c9da295044d13897a22
unknown <jorammanoah1@gmail.com>
2021-04-26 15:05:14 -0700

    Finished dispatch action

--- src/Products/Product.js
+++ src/Products/Product.js
@@ -1,20 +1,42 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { categoryProduct } from '../_actions/productActions'
+import { Col, Empty, Row } from "antd";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { categoryProduct } from "../_actions/productActions";
 
 export default function Product(props) {
-    const dispatch = useDispatch()
+  const categoryDetail = useSelector((state) => state.categoryList);
+  const { loadingCategory, products, errorCategory } = categoryDetail;
 
-    console.log(props.match.params.category)
-    useEffect(() => {
-        dispatch(categoryProduct(props.match.params.category))
-        return () => {
-            
-        }
-    }, [])
-    return (
+  const dispatch = useDispatch();
+
+  console.log(props.match.params.category);
+  useEffect(() => {
+    dispatch(categoryProduct(props.match.params.category));
+    return () => {};
+  }, []);
+  return (
+    <div style={{ marginTop: "5rem" }}>
+      {loadingCategory ? (
+        <div>Loading</div>
+      ) : errorCategory ? (
+        <div>error</div>
+      ) : (
         <div>
-            
+          {products.length === 0 ? (
+            <Row justify="space-around" align="middle">
+              <Col>
+                <Empty description="No category" />
+              </Col>
+            </Row>
+          ) : (
+            <div>
+              {products.map((item) => (
+                <h1>{item.product_name}</h1>
+              ))}
+            </div>
+          )}
         </div>
-    )
+      )}
+    </div>
+  );
 }
