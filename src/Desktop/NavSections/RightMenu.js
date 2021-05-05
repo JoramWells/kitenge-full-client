a01f6ce1537d7a3a0270f61e500552a3496fcd00
unknown <jorammanoah1@gmail.com>
2021-05-05 01:25:18 -0700

    Product commit

--- src/Desktop/NavSections/RightMenu.js
+++ src/Desktop/NavSections/RightMenu.js
@@ -1,7 +1,18 @@
 import React, { useState } from "react";
-import { Menu, Space, Badge, Modal, Col, Row, Typography, Image,message, Button } from "antd";
+import {
+  Menu,
+  Space,
+  Badge,
+  Modal,
+  Col,
+  Row,
+  Typography,
+  Image,
+  message,
+  Button,
+} from "antd";
 import Cookie from "js-cookie";
-import {withRouter,Link, useHistory} from "react-router-dom"
+import { withRouter, Link, useHistory } from "react-router-dom";
 import {
   EditOutlined,
   LoginOutlined,
@@ -19,50 +30,60 @@ const { Text, Title } = Typography;
 const { SubMenu } = Menu;
 // const cartItems = Cookie.getJSON("cartItems");
 
-
 function RightMenu() {
-  const history = useHistory()
+  const history = useHistory();
   const userSignin = useSelector((state) => state.userSignin);
-  const CartItems = useSelector((state)=>state.cart)
-  const {cartItems} = CartItems
+  const CartItems = useSelector((state) => state.cart);
+  const { cartItems } = CartItems;
   const { userInfo } = userSignin;
   const [visible, setVisible] = useState(false);
-//   const PaymentList = useSelector((state) => state.paymentList);
-//   const { payments } = PaymentList;
+  //   const PaymentList = useSelector((state) => state.paymentList);
+  //   const { payments } = PaymentList;
 
-  function showModal (){
+  function showModal() {
     setVisible(true);
-  };
-   function logout(){
-    if(!userInfo){
-      console.log()
+  }
+  function logout() {
+    if (!userInfo) {
+      console.log();
     }
-    Cookie.remove('userInfo')
-    setTimeout((
-      message.success('Logged out successfully')
-    ),1000)
-    return history.goBack()
+    Cookie.remove("userInfo");
+    setTimeout(message.success("Logged out successfully"), 1000);
+    history.goBack();
   }
 
-  function  handleOk (){
+  function handleOk() {
     setVisible(false);
-  };
-  function handleCancel (){
+  }
+  function handleCancel() {
     setVisible(false);
-  };
+  }
 
   if (!userInfo) {
     return (
-      <Menu mode="horizontal" style={{paddingTop:"0.4rem"}}>
+      <Menu mode="horizontal" style={{ paddingTop: "0.4rem" }}>
         <Menu.Item key="mail">
           <Link to="/login">
-          <Button icon={<LoginOutlined style={{margin:"0px", fontSize:"1rem"}} />} style={{borderRadius:"50px"}}>Signin</Button>
-            
+            <Button
+              icon={
+                <LoginOutlined style={{ margin: "0px", fontSize: "1rem" }} />
+              }
+              style={{ borderRadius: "50px" }}
+            >
+              Signin
+            </Button>
           </Link>
         </Menu.Item>
         <Menu.Item key="app">
           <Link to="/register">
-            <Button style={{borderRadius:"50px"}} icon={<UserAddOutlined style={{fontSize:"1rem", margin:"0px"}} />}>Signup</Button>
+            <Button
+              style={{ borderRadius: "50px" }}
+              icon={
+                <UserAddOutlined style={{ fontSize: "1rem", margin: "0px" }} />
+              }
+            >
+              Signup
+            </Button>
           </Link>
         </Menu.Item>
       </Menu>
@@ -72,134 +93,151 @@ function RightMenu() {
       return (
         <Space>
           <Menu mode="horizontal">
-
-            <SubMenu title={
-              <ShoppingCartOutlined
+            <SubMenu
+              title={
+                <ShoppingCartOutlined
                   onClick={showModal}
-
-                  style={{ fontSize: "1.5rem", margin:"0", color:"grey"}}
-
-              />
-            }>
-
-            </SubMenu>
+                  style={{ fontSize: "1.5rem", margin: "0", color: "grey" }}
+                />
+              }
+            ></SubMenu>
 
             <SubMenu
-              title={<QuestionCircleOutlined style={{ fontSize: "1.3rem",marginTop:"0" }} />}
+              title={
+                <QuestionCircleOutlined
+                style={{ fontSize: "1.3rem",  color:"grey"}}
+                />
+              }
             >
-              <Menu.Item style={{margin:"0px"}} >Contact supplier</Menu.Item>
-              <Menu.Item style={{margin:"0px"}}>How to add product</Menu.Item>
+              <Menu.Item style={{ margin: "0px" }}>Contact supplier</Menu.Item>
+              <Menu.Item style={{ margin: "0px" }}>
+                How to add product
+              </Menu.Item>
               <Menu.Item>Creating account</Menu.Item>
               <Menu.Item>Tracking your order</Menu.Item>
-
-
             </SubMenu>
             <SubMenu
-                style={{  borderRadius: "10px", marginLeft:"0" }}
-                title={<SettingOutlined style={{ fontSize: "1.3rem" }} />}
-              >
-            <Menu.Item icon={<PlusOutlined/>}>
-              <Link to="/products/add">
-              Add Product
-              </Link>
-               
+              style={{ borderRadius: "10px", marginLeft: "0" }}
+              title={<SettingOutlined  style={{ fontSize: "1.3rem", margin:"0", color:"grey"}} />}
+            >
+              <Menu.Item icon={<PlusOutlined />}>
+                <Link to="/products/add">Add Product</Link>
               </Menu.Item>
               <Menu.Item icon={<EditOutlined />}>
-              <Link to="/produc/manage">
-              Edit Product
-              </Link>
-               
+                <Link to="/produc/manage">Edit Product</Link>
               </Menu.Item>
-              
+            </SubMenu>
 
-
-              </SubMenu>
-
-            <SubMenu style={{marginLeft:"0"}} title={<Avatar src={userInfo.avatar} style={{width:"25px", height:"auto",marginBottom:"0.5rem", marginLeft:"0"}} />}>
-
+            <SubMenu
+              style={{ marginLeft: "0" }}
+              title={
+                <Avatar
+                  src={userInfo.avatar}
+                  style={{
+                    width: "25px",
+                    height: "auto",
+                    marginBottom: "0.5rem",
+                    marginLeft: "0",
+                  }}
+                />
+              }
+            >
               <Menu.Item>{userInfo.email}</Menu.Item>
-              <Menu.Item onClick={()=>logout()}>Logout</Menu.Item>
+              <Menu.Item onClick={() => logout()}>Logout</Menu.Item>
             </SubMenu>
           </Menu>
-
         </Space>
       );
     } else {
       return (
         <Space>
           <Menu mode="horizontal">
-
             <Menu.Item>
               <Badge count={cartItems.length}>
                 <ShoppingCartOutlined
                   onClick={showModal}
-                  style={{ fontSize: "1.4rem", margin:"0" }}
+                  style={{ fontSize: "1.4rem", margin:"0", color:"grey"}}
                 />
               </Badge>
-              
             </Menu.Item>
             <Menu.Item>
               <Badge dot count={cartItems.length}>
                 <MailOutlined
-                  style={{ fontSize: "1.3rem", margin:"0" }}
+                  style={{ fontSize: "1.3rem", margin: "0", color: "grey" }}
                 />
               </Badge>
-              
             </Menu.Item>
 
             <SubMenu
-              title={<QuestionCircleOutlined style={{ fontSize: "1.3rem" }} />}
+              title={<QuestionCircleOutlined  style={{ fontSize: "1.3rem",  color:"grey"}} />}
             >
-              <Menu.Item style={{margin:"0px"}} >Contact supplier</Menu.Item>
-              <Menu.Item style={{margin:"0px"}}>How to add product</Menu.Item>
+              <Menu.Item style={{ margin: "0px" }}>Contact supplier</Menu.Item>
+              <Menu.Item style={{ margin: "0px" }}>
+                How to add product
+              </Menu.Item>
               <Menu.Item>Creating account</Menu.Item>
               <Menu.Item>Tracking your order</Menu.Item>
-
-
             </SubMenu>
             <SubMenu
-                style={{ borderRadius: "10px", marginLeft:"0" }}
-                title={<SettingOutlined style={{ fontSize: "1.3rem" }} />}
-              >
-            <Menu.Item icon={<PlusOutlined/>}>
-              <Link to="/products/add">
-              Add Product
-              </Link>
-               
+              style={{ borderRadius: "10px", marginLeft: "0" }}
+              title={<SettingOutlined  style={{ fontSize: "1.3rem", color:"grey"}} />}
+            >
+              <Menu.Item icon={<PlusOutlined />}>
+                <Link to="/products/add">Add Product</Link>
               </Menu.Item>
-              <Menu.Item icon={<EditOutlined/>}>
-              <Link to="/produc/manage">
-              Edit Product
-              </Link>
-               
+              <Menu.Item icon={<EditOutlined />}>
+                <Link to="/produc/manage">Edit Product</Link>
               </Menu.Item>
-
-
-              </SubMenu>
-            <SubMenu style={{marginLeft:"0"}} title={<Avatar   src={userInfo.avatar} style={{marginBottom:"0.5rem", width:"25px", height:"inherit"}} />}>
-
+            </SubMenu>
+            <SubMenu
+              style={{ marginLeft: "0" }}
+              title={
+                <Avatar
+                  src={userInfo.avatar}
+                  style={{
+                    marginBottom: "0.5rem",
+                    width: "25px",
+                    height: "inherit",
+                  }}
+                />
+              }
+            >
               <Menu.Item>{userInfo.email}</Menu.Item>
-              <Menu.Item style={{justifyContent:"space-around", display:"flex", margin:"0"}}>
-                <Button style={{borderRadius:"50px"}}>
+              <Menu.Item
+                style={{
+                  justifyContent: "space-around",
+                  display: "flex",
+                  margin: "0",
+                }}
+              >
+                <Button style={{ borderRadius: "50px" }}>
                   <Link to="/register">Manage account</Link>
                 </Button>
               </Menu.Item>
-              
-              <Menu.Item style={{justifyContent:"space-around", display:"flex", margin:"0"}}>Logout</Menu.Item>
+
+              <Menu.Item
+                style={{
+                  justifyContent: "space-around",
+                  display: "flex",
+                  margin: "0",
+                }}
+              >
+                Logout
+              </Menu.Item>
             </SubMenu>
           </Menu>
-          <Modal
-            visible={visible}
-            onOk={handleOk}
-            onCancel={handleCancel}
-          >
+          <Modal visible={visible} onOk={handleOk} onCancel={handleCancel}>
             {cartItems.map((product) => (
               <Row justify="space-around" align="middle" key={product.image}>
                 <Col>
                   <Image
-                    src={"/"+product.image}
+                    src={"/" + product.image}
                     alt="profile_pic"
-                    style={{ width: "50px", height:"50px", objectFit:"contain" }}
+                    style={{
+                      width: "50px",
+                      height: "50px",
+                      objectFit: "contain",
+                    }}
                   />
                 </Col>
                 <Col>
@@ -217,4 +255,4 @@ function RightMenu() {
   }
 }
 
-export default withRouter (RightMenu);
+export default withRouter(RightMenu);
