8d3714e2e1f0ebcdbd0482285d127ab9a2ec2887
bramuel <jorammanoah1@gmail.com>
2021-06-17 15:02:58 -0700

    Removed crousel

--- src/Desktop/CarouselItems.js
+++ src/Desktop/CarouselItems.js
@@ -1,18 +1,12 @@
 import React, { useEffect, memo } from "react";
-import Carousel from "react-multi-carousel";
-import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../_actions/productActions";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Row, Col, Card, Result, Button } from "antd";
 import RecentItemsBar from "../Generic/RecentItemsBar";
-import {
-  EyeOutlined,
-  HeartFilled,
-  LikeFilled,
-  RedoOutlined,
-} from "@ant-design/icons";
-import NumberFormat from "react-number-format";
+import { RedoOutlined } from "@ant-design/icons";
+// import NumberFormat from "react-number-format";
+import { DotsVerticalIcon, ThumbUpIcon } from "@heroicons/react/solid";
 
 const renderSkeleton = [...Array(5).keys()].map((i) => {
   return (
@@ -56,7 +50,6 @@ export function CarouselItem() {
   }, [dispatch]);
   return (
     <>
-
       <RecentItemsBar title="Most popular" />
       {loading ? (
         <Row
@@ -77,207 +70,129 @@ export function CarouselItem() {
           }
         />
       ) : (
-        <div style={{ maxWidth: "85%", margin: "auto", display: "block" }}>
-          <Carousel
-            swipeable={true}
-            draggable={true}
-            responsive={responsive}
-            infinite={true}
-            autoPlay={false}
-            arrows={false}
-          
-          >
-            {posts.map((product) => (
-              <div
-                key={product.id}
-                className="flex items-center p-2 content-center bg-white rounded-md mb-4 mt-4"
-              >
-                <Link
-                  to={`/product-detail/${product.id}/?category=${product.category}`}
-                  style={{ textDecoration: "none" }}
-                >
-                  <Card
-                    style={{
-                      width: "15rem",
-                      height: "290px",
-                      border: "none",
-                    }}
-                    cover={
-                      <LazyLoadImage
-                        src={product.image}
-                        effect="blur"
-                        alt="productimage"
-                        style={{
-                          width: "14.8rem",
-                          height: "10.5rem",
-                          display: "flex",
-                          margin: "auto",
-                        }}
-                      />
-                    }
-                    className="hover:shadow-lg hover:rounded-md"
-                  >
-                    <Link
-                      to={`/product-detail/${product.id}/?category=${product.category}`}
-                      className="m-0 text-gray-700 text-sm"
-                    >{product.product_name}
-
-                    </Link>
-                    {/* <Rate
-                      allowHalf={true}
-                      style={{
-                        fontSize: "1rem",
-                        color: "#434343",
-
-                        marginBottom: ".6rem",
-                      }}
-                      defaultValue={product.ratings}
-                    /> */}
-                    <p
-                    className="text-gray-700 font-medium m-0"
-
-                    >
-                      <NumberFormat
-                        value={product.price}
-                        thousandSeparator={true}
-                        displayType={"text"}
-                        prefix="Kshs: "
-                        suffix=" /="
-                      />
-                    </p>
-                    {/* <Divider style={{ margin: "0.7rem" }} /> */}
-
-                    <div
-                    className="flex space-x-4 items-center content-center bg-gray-50 rounded-sm p-1"
-                    >
-                      <div className="flex items-center">
-                        <EyeOutlined style={{ color: "grey" }} />{" "}
-                        <LikeFilled style={{ color: "#bfbfbf" }} />{" "}
-                        <span style={{ color: "grey" }}>{product.likes}</span>
-                      </div>
-                      <div className="text-gray-300">Sold 93</div>
-                    </div>
-                  </Card>
-                </Link>
+        <div style={{width:"90%"}} className="flex flex-wrap space-x-10 items-center justify-center">
+          {posts.map((product) => (
+            <figure
+              style={{ width: "14rem", border: "1px solid #F0F0F0 " }}
+              className="rounded-md bg-white"
+            >
+              <div className="absolute bg-black bg-opacity-20  text-white p-1 rounded-full flex justify-end flex-row items-end focus:bg-opacity-20 active:bg-opacity-20 ">
+                <DotsVerticalIcon
+                  className="h-5"
+                  //  onClick={() => setOpen(true)}
+                />
               </div>
-            ))}
-          </Carousel>
-        </div>
-      )}
-
-      <RecentItemsBar title="Available Now!!" />
-      {loading ? (
-        <Row
-          justify="space-around"
-          align="middle"
-          style={{ marginTop: "1rem" }}
-        >
-          {renderSkeleton}
-        </Row>
-      ) : error ? (
-        <Result
-          status="500"
-          subTitle={error}
-          extra={
-            <Button onClick={reloadHandler} icon={<RedoOutlined />}>
-              RETRY
-            </Button>
-          }
-        />
-      ) : (
-        <div style={{ maxWidth: "85%", margin: "auto", display: "block" }}>
-          <Carousel
-            swipeable={true}
-            draggable={true}
-            responsive={responsive}
-            infinite={true}
-            autoPlay={false}
-            arrows={false}
-            renderButtonGroupOutside={true}
-          >
-            {posts.map((product) => (
-              <Row
-                key={product.id}
-                justify="center"
+              <LazyLoadImage
+                src={product.image}
+                effect="blur"
+                alt="productimage"
                 style={{
-                  marginTop: "1rem",
-                  marginBottom: "1rem",
-                  alignItems: "center",
-                  backgroundColor:"white",
-                  borderRadius:"5px",
-                  padding: "1rem"
+                  width: "14rem",
+                  height: "135px",
+                  display: "block",
+                  margin: "auto",
                 }}
-              >
-                <Link
-                  to={`/product-detail/${product.id}/?category=${product.category}`}
-                  style={{ textDecoration: "none" }}
-                >
-                  <Card
-                    style={{
-                      width: "15rem",
-                      height: "290px",
-                      border: "none",
-                    }}
-                    cover={
-                      <LazyLoadImage
-                        src={product.image}
-                        effect="blur"
-                        alt="productimage"
-                        style={{
-                          width: "14.8rem",
-                          height: "10.5rem",
-                          display: "flex",
-                          margin: "auto",
-                        }}
-                      />
-                    }
-                    className="hover:shadow-lg hover:rounded-md"
-                  >
-                    <Link
-                      to={`/product-detail/${product.id}/?category=${product.category}`}
-                      className="m-0 text-gray-700 text-sm"
-                    >{product.product_name}
+                className="rounded-t-sm"
+              />
+              <div className="p-2">
+                {/* <blockquote style={{hid:"hidden"}} >{id}</blockquote> */}
 
-                    </Link>
-                    {/* <Rate
-                      allowHalf={true}
-                      style={{
-                        fontSize: "1rem",
-                        color: "#434343",
-
-                        marginBottom: ".6rem",
-                      }}
-                      defaultValue={product.ratings}
-                    /> */}
-                    <p
-                    className="text-gray-700 font-medium m-0"
-
-                    >
-                      <NumberFormat
-                        value={product.price}
-                        thousandSeparator={true}
-                        displayType={"text"}
-                        prefix="Kshs: "
-                        suffix=" /="
-                      />
+                <p style={{ color: "#00ADAD" }}>{product.product_name}</p>
+                <figcaption>
+                  <div>
+                    <p className="font-semibold text-gray-600">
+                      Kshs {product.price} /=
                     </p>
-                    {/* <Divider style={{ margin: "0.7rem" }} /> */}
+                  </div>
+                  <div className="line-through text-sm text-gray-300">
+                    Kshs 4,321/=
+                    {product.likes}
+                  </div>
+                  <div className="flex flex-row items-center justify-between content-center mt-2 text-gray-400">
+                    <div className="text-sm">12mins</div>
+                    <div className="flex flex-row items-center">
+                      <div className="text-sm">{product.id}31</div>
 
-                    <div
-                    className="flex space-x-4 items-center content-center bg-gray-50 rounded-sm p-1"
-                    >
-                      <div className="flex items-center">
-                        <EyeOutlined style={{ color: "grey" }} />{" "}
-                        <LikeFilled style={{ color: "#bfbfbf" }} />{" "}
-                        <span style={{ color: "grey" }}>{product.likes}</span>
-                      </div>
-                      <div className="text-gray-300">Sold 93</div>
+                      <ThumbUpIcon className="h-5 " />
                     </div>
-                  </Card>
-                </Link>
-              </Row>
-            ))}
-          </Carousel>
+                  </div>
+                </figcaption>
+              </div>
+            </figure>
+            // <div
+            //   key={product.id}
+            //   className="flex items-center p-2 content-center bg-white rounded-md mb-4 mt-4"
+            // >
+            //   <Link
+            //     to={`/product-detail/${product.id}/?category=${product.category}`}
+            //     style={{ textDecoration: "none" }}
+            //   >
+            //     <Card
+            //       style={{
+            //         width: "15rem",
+            //         height: "290px",
+            //         border: "none",
+            //       }}
+            //       cover={
+            //         <LazyLoadImage
+            //           src={product.image}
+            //           effect="blur"
+            //           alt="productimage"
+            //           style={{
+            //             width: "14.8rem",
+            //             height: "10.5rem",
+            //             display: "flex",
+            //             margin: "auto",
+            //           }}
+            //         />
+            //       }
+            //       className="hover:shadow-lg hover:rounded-md"
+            //     >
+            //       <Link
+            //         to={`/product-detail/${product.id}/?category=${product.category}`}
+            //         className="m-0 text-gray-700 text-sm"
+            //       >{product.product_name}
+
+            //       </Link>
+            //       {/* <Rate
+            //         allowHalf={true}
+            //         style={{
+            //           fontSize: "1rem",
+            //           color: "#434343",
+
+            //           marginBottom: ".6rem",
+            //         }}
+            //         defaultValue={product.ratings}
+            //       /> */}
+            //       <p
+            //       className="text-gray-700 font-medium m-0"
+
+            //       >
+            //         <NumberFormat
+            //           value={product.price}
+            //           thousandSeparator={true}
+            //           displayType={"text"}
+            //           prefix="Kshs: "
+            //           suffix=" /="
+            //         />
+            //       </p>
+            //       {/* <Divider style={{ margin: "0.7rem" }} /> */}
+
+            //       <div
+            //       className="flex space-x-4 items-center content-center bg-gray-50 rounded-sm p-1"
+            //       >
+            //         <div className="flex items-center">
+            //           <EyeOutlined style={{ color: "grey" }} />{" "}
+            //           <LikeFilled style={{ color: "#bfbfbf" }} />{" "}
+            //           <span style={{ color: "grey" }}>{product.likes}</span>
+            //         </div>
+            //         <div className="text-gray-300">Sold 93</div>
+            //       </div>
+            //     </Card>
+            //   </Link>
+            // </div>
+          ))}
+          {/* </Carousel> */}
         </div>
       )}
     </>
