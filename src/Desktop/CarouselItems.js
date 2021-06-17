6a196c2bec0242b7707f72daa2a5d38575ff8cce
bramuel <jorammanoah1@gmail.com>
2021-06-17 15:48:59 -0700

    Finished landing pge

--- src/Desktop/CarouselItems.js
+++ src/Desktop/CarouselItems.js
@@ -70,7 +70,10 @@ export function CarouselItem() {
           }
         />
       ) : (
-        <div style={{width:"90%"}} className="flex flex-wrap space-x-10 items-center justify-center">
+        <div
+          style={{ width: "85%", float: "right", padding: "1.55rem" }}
+          className="flex flex-row flex-wrap justify-between items-center "
+        >
           {posts.map((product) => (
             <figure
               style={{ width: "14rem", border: "1px solid #F0F0F0 " }}
@@ -95,104 +98,19 @@ export function CarouselItem() {
                 className="rounded-t-sm"
               />
               <div className="p-2">
-                {/* <blockquote style={{hid:"hidden"}} >{id}</blockquote> */}
-
-                <p style={{ color: "#00ADAD" }}>{product.product_name}</p>
+                {product.product_name}
                 <figcaption>
-                  <div>
-                    <p className="font-semibold text-gray-600">
+                  <div className="font-semibold text-gray-600">
                       Kshs {product.price} /=
-                    </p>
                   </div>
                   <div className="line-through text-sm text-gray-300">
                     Kshs 4,321/=
-                    {product.likes}
                   </div>
-                  <div className="flex flex-row items-center justify-between content-center mt-2 text-gray-400">
-                    <div className="text-sm">12mins</div>
-                    <div className="flex flex-row items-center">
-                      <div className="text-sm">{product.id}31</div>
 
-                      <ThumbUpIcon className="h-5 " />
-                    </div>
-                  </div>
                 </figcaption>
               </div>
             </figure>
-            // <div
-            //   key={product.id}
-            //   className="flex items-center p-2 content-center bg-white rounded-md mb-4 mt-4"
-            // >
-            //   <Link
-            //     to={`/product-detail/${product.id}/?category=${product.category}`}
-            //     style={{ textDecoration: "none" }}
-            //   >
-            //     <Card
-            //       style={{
-            //         width: "15rem",
-            //         height: "290px",
-            //         border: "none",
-            //       }}
-            //       cover={
-            //         <LazyLoadImage
-            //           src={product.image}
-            //           effect="blur"
-            //           alt="productimage"
-            //           style={{
-            //             width: "14.8rem",
-            //             height: "10.5rem",
-            //             display: "flex",
-            //             margin: "auto",
-            //           }}
-            //         />
-            //       }
-            //       className="hover:shadow-lg hover:rounded-md"
-            //     >
-            //       <Link
-            //         to={`/product-detail/${product.id}/?category=${product.category}`}
-            //         className="m-0 text-gray-700 text-sm"
-            //       >{product.product_name}
-
-            //       </Link>
-            //       {/* <Rate
-            //         allowHalf={true}
-            //         style={{
-            //           fontSize: "1rem",
-            //           color: "#434343",
-
-            //           marginBottom: ".6rem",
-            //         }}
-            //         defaultValue={product.ratings}
-            //       /> */}
-            //       <p
-            //       className="text-gray-700 font-medium m-0"
-
-            //       >
-            //         <NumberFormat
-            //           value={product.price}
-            //           thousandSeparator={true}
-            //           displayType={"text"}
-            //           prefix="Kshs: "
-            //           suffix=" /="
-            //         />
-            //       </p>
-            //       {/* <Divider style={{ margin: "0.7rem" }} /> */}
-
-            //       <div
-            //       className="flex space-x-4 items-center content-center bg-gray-50 rounded-sm p-1"
-            //       >
-            //         <div className="flex items-center">
-            //           <EyeOutlined style={{ color: "grey" }} />{" "}
-            //           <LikeFilled style={{ color: "#bfbfbf" }} />{" "}
-            //           <span style={{ color: "grey" }}>{product.likes}</span>
-            //         </div>
-            //         <div className="text-gray-300">Sold 93</div>
-            //       </div>
-            //     </Card>
-            //   </Link>
-            // </div>
           ))}
-          {/* </Carousel> */}
         </div>
       )}
     </>
